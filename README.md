acaeb53e4745acd17ba07a5cb64681c4a094aa8b
Aké_Esso <68826490+Esso17@users.noreply.github.com>
2023-04-23 13:50:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pipelines
+Ai pipelines for training 
---
+++ python/dowload_data.py
@@ -0,0 +1,31 @@
+
+
+import json
+import argparse
+from pathlib import Path
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+def _download_data(args):
+    x, y = load_breast_cancer(return_X_y=True)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    data = {'x_train' : x_train.tolist(),
+            'y_train' : y_train.tolist(),
+            'x_test' : x_test.tolist(),
+            'y_test' : y_test.tolist()}
+
+    data_json = json.dumps(data)
+
+
+    with open(args.data, 'w') as out_file:
+        json.dump(data_json, out_file)
+        
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str)
+    args = parser.parse_args()
+    Path(args.data).parent.mkdir(parents=True, exist_ok=True)
+    _download_data(args)
+    
+    
+  
